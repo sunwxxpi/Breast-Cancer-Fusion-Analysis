ace90f4f5bbf2a8e8f8fe75235ac97cb96671c86
Sunwoo Pi <ppssww1202@naver.com>
2024-05-05 15:13:58 +0000

    fix: :bug: Change algorithm for extracting exact elastogram region

--- util/elastogram_extractor.py
+++ util/elastogram_extractor.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import glob
 
-def crop_strain_region(image):
+def crop_strain_elastography(image):
     lower_black = np.array([0, 0, 0])
     upper_black = np.array([15, 15, 15])
     mask = cv2.inRange(image, lower_black, upper_black)
@@ -25,7 +25,7 @@ def crop_strain_region(image):
     return x, y, w, h, cropped_image
 
 
-def crop_largest_rgb_region(image):
+def crop_elastogram(image):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_bound = np.array([0, 50, 50])
     upper_bound = np.array([179, 255, 255])
@@ -37,16 +37,16 @@ def crop_largest_rgb_region(image):
         largest_contour = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(largest_contour)
         
-        x = x + 5
-        y = y + 5
-        w = w - 10
-        h = h - 10
+        x = x + 15
+        y = y + 15
+        w = w - 30
+        h = h - 30
         cropped_image = image[y:y+h, x:x+w]
 
     return x, y, w, h, cropped_image
 
 
-def crop_elastography_region_from_grayscale(image, sr_x, sr_y, brr_x, brr_y, brr_w, brr_h):
+def crop_elastogram_from_grayscale(image, sr_x, sr_y, brr_x, brr_y, brr_w, brr_h):
     target_x = sr_x + brr_x
     target_y = sr_y + brr_y
     target_w = brr_w
@@ -58,11 +58,10 @@ def crop_elastography_region_from_grayscale(image, sr_x, sr_y, brr_x, brr_y, brr
 
 
 def find_rightmost_rgb_column(image):
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    color_mask = cv2.inRange(hsv_image, (0, 50, 50), (179, 255, 255))
-    
+    color_mask = (image[:,:,0] != image[:,:,1]) | (image[:,:,1] != image[:,:,2]) | (image[:,:,0] != image[:,:,2])
+
     width = image.shape[1]
-    
+
     for x in range(width-1, -1, -1):
         if np.any(color_mask[:, x]):
             return x
@@ -70,12 +69,11 @@ def find_rightmost_rgb_column(image):
     return -1
 
 
-def find_bottommost_gray_column(image):
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    gray_mask = cv2.inRange(hsv_image, (0, 0, 50), (179, 50, 220))
-    
+def find_bottommost_gray_row(image):
+    gray_mask = (image[:,:,0] == image[:,:,1]) & (image[:,:,1] == image[:,:,2])
+
     height = image.shape[0]
-    
+
     for y in range(height-1, -1, -1):
         if np.any(gray_mask[y, :]):
             return y
@@ -102,17 +100,17 @@ def ensure_dir(directory):
         os.makedirs(directory)
 
 
-def crop_elastography_region(base_dir, output_dir):
+def extract_elastogram(base_dir, output_dir):
     for score in range(1, 6):
         input_path = f"{base_dir}/score{score}/*.png"
         images = glob.glob(input_path)
         
         for image_path in images:
             try:
-                output_subdir = os.path.join(output_dir, f"elastography_region/score{score}")
+                output_subdir = os.path.join(output_dir, f"elastogram_cropped/score{score}")
                 ensure_dir(output_subdir)
                 
-                grayscale_output_subdir = os.path.join(output_dir, f"elastography_region_from_grayscale/score{score}")
+                grayscale_output_subdir = os.path.join(output_dir, f"elastogram_from_grayscale_cropped/score{score}")
                 ensure_dir(grayscale_output_subdir)
                 
                 file_name = os.path.basename(image_path)
@@ -121,23 +119,23 @@ def crop_elastography_region(base_dir, output_dir):
                 
                 image = cv2.imread(image_path)
                 
-                sr_x, sr_y, _, _, strain_region = crop_strain_region(image)
-                brr_x, brr_y, brr_w, brr_h, biggest_rgb_region = crop_largest_rgb_region(strain_region)
+                sr_x, sr_y, _, _, strain_region = crop_strain_elastography(image)
                 
-                elastography_region_from_grayscale = crop_elastography_region_from_grayscale(image, sr_x, sr_y, brr_x, brr_y, brr_w, brr_h)
+                brr_x, brr_y, brr_w, brr_h, elastogram = crop_elastogram(strain_region)
+                elastogram_from_grayscale = crop_elastogram_from_grayscale(image, sr_x, sr_y, brr_x, brr_y, brr_w, brr_h)
                 
-                offset_x = find_rightmost_rgb_column(elastography_region_from_grayscale)
-                image_rgb_cropped = remove_left_rgb_part(biggest_rgb_region, offset_x)
-                image_gray_cropped = remove_left_rgb_part(elastography_region_from_grayscale, offset_x)
+                offset_x = find_rightmost_rgb_column(elastogram_from_grayscale)
+                elastogram_cropped = remove_left_rgb_part(elastogram, offset_x)
+                elastogram_from_grayscale_cropped = remove_left_rgb_part(elastogram_from_grayscale, offset_x)
+                
+                offset_y = find_bottommost_gray_row(elastogram_cropped)
+                elastogram_cropped = remove_top_gray_part(elastogram_cropped, offset_y)
+                elastogram_from_grayscale_cropped = remove_top_gray_part(elastogram_from_grayscale_cropped, offset_y)                
 
-                # offset_y = find_bottommost_gray_column(image_rgb_cropped)
-                # image_rgb_cropped = remove_top_gray_part(image_rgb_cropped, offset_y)
-                # image_gray_cropped = remove_top_gray_part(image_gray_cropped, offset_y)
-
-                cv2.imwrite(output_path, image_rgb_cropped)
+                cv2.imwrite(output_path, elastogram_cropped)
                 print(f"Saved {output_path}")
                 
-                cv2.imwrite(grayscale_output_path, image_gray_cropped)
+                cv2.imwrite(grayscale_output_path, elastogram_from_grayscale_cropped)
                 print(f"Saved {grayscale_output_path}")
                 
             except Exception as e:
@@ -146,4 +144,4 @@ def crop_elastography_region(base_dir, output_dir):
 
 base_dir = './strain_png/S'
 output_dir = './strain_png'
-crop_elastography_region(base_dir, output_dir)
+extract_elastogram(base_dir, output_dir)
